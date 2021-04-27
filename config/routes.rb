0117fbbfa0f9073d9e1d76a3553a74e664908730
teramoto-chihiro <komorikiri1031@gmail.com>
2021-04-27 12:07:42 +0000

    エラー解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'users# new'
-   resources :users, only: [:new, :create, :show]
+   resources :users, only: [:new, :create, :show, :edit]
    resources :sessions, only: [:new, :create, :destroy]
-   resources :picture_blogs do
+   resources :pictures do
     collection do
      post :confirm
     end
